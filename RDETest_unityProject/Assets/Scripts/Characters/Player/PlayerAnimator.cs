2897bf2f9c4d0caa832be30844163c3c2f37879d
Xomrac <morettimarco2759@gmail.com>
2025-10-28 15:35:06 +0100

    Add: Player's idle and running animations

--- RDETest_unityProject/Assets/Scripts/Characters/Player/PlayerAnimator.cs
+++ RDETest_unityProject/Assets/Scripts/Characters/Player/PlayerAnimator.cs
@@ -8,6 +8,9 @@ namespace RDE.Characters.PlayerCharacter
 
 	public class PlayerAnimator : A2DCharacterAnimator
 	{
+		private static readonly int Moving = Animator.StringToHash("Moving");
+		private static readonly int Y = Animator.StringToHash("Y");
+		private static readonly int X = Animator.StringToHash("X");
 		private PlayerMover _playerMover;
 		protected override void Start()
 		{
@@ -21,23 +24,21 @@ namespace RDE.Characters.PlayerCharacter
 
 		private void OnCharacterStopped(Vector2 previousDirection)
 		{
+            _animator.SetBool(Moving, false);
+			PlayDirectionalAnimation(previousDirection);
 		}
 
 		private void OnCharacterMoved(Vector2 movementDirection)
 		{
+			_animator.SetBool(Moving, true);
 			PlayDirectionalAnimation(movementDirection);
 		}
 		
 
 		public override void PlayDirectionalAnimation(Vector2 direction)
 		{
-			_animator.SetFloat("x", direction.x);
-			_animator.SetFloat("y", direction.y);
-		}
-
-		private void PlayIdleAnimation(Vector2 direction)
-		{
-			
+			_animator.SetFloat(X, direction.x);
+			_animator.SetFloat(Y, direction.y);
 		}
 	}
 
