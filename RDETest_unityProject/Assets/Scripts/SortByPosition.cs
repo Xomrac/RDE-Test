456a4b5dd9cc08a225106c45e7d76b67b4247771
Xomrac <morettimarco2759@gmail.com>
2025-10-29 22:52:22 +0100

    Add: obstacles to level
    Struggled a lot with Isometric sorting and setupping the tilemaps (very bad asset)

---
+++ RDETest_unityProject/Assets/Scripts/SortByPosition.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine.Rendering;
+
+namespace RDE
+{
+	using UnityEngine;
+
+	[RequireComponent(typeof(SpriteRenderer))]
+	[RequireComponent(typeof(SortingGroup))]
+	public class SortByPosition : MonoBehaviour
+	{
+		[Tooltip("Offset to add to the calculated order")]
+		[SerializeField] private int _orderOffset = 0;
+		[Tooltip("Bigger = more precise, but more sensitive")]
+		[SerializeField] private int _precision = 100;
+		[Tooltip("Point from which to calculate sorting")]
+		[SerializeField] private Transform _sortPoint;
+
+		private SortingGroup _group;
+
+		private void Awake()
+		{
+			_group = GetComponent<SortingGroup>();
+			
+			if (_sortPoint == null) _sortPoint = transform;
+		}
+
+		private void Update()
+		{
+			float y = -_sortPoint.position.y;
+			int order = Mathf.RoundToInt(y * _precision) + _orderOffset;
+			_group.sortingOrder = order;
+		}
+	}
+
+
+}
