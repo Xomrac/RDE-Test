acdcdc3282c077b3d0140fcef57dc906a2189bf6
Xomrac <morettimarco2759@gmail.com>
2025-11-01 21:52:01 +0100

    Fix: The enemy not stopping when entering attack state

--- RDETest_unityProject/Assets/Scripts/Characters/Enemies/FSMStates/AttackState.cs
+++ RDETest_unityProject/Assets/Scripts/Characters/Enemies/FSMStates/AttackState.cs
@@ -1,14 +1,25 @@
-﻿using UnityEngine;
+﻿using RDETest.Characters.Characters.Enemies;
+using UnityEngine;
 using XomracCore.FSM;
+using XomracCore.Patterns.SL;
 
 namespace RDE.Characters.Enemies.FMSStates
 {
 
 	public class AttackState : IState
 	{
+		private Enemy _enemy;
+		private EnemyMover _enemyMover;
+		public AttackState(Enemy enemy)
+		{
+			_enemy = enemy;
+			_enemyMover = ServiceLocator.Of(enemy).GetService<EnemyMover>();
+		}
 
 		public void Enter()
 		{
+			_enemyMover.Stop();
+			_enemy.ChangeColor(Color.red);
 		}
 
 		public void Update()
